c2c8f73cce536438e98c21be10e3a27a758d0d3a
ayoub <ayoubaitabidalla1@gmail.com>
2024-08-28 09:20:03 +0100

    implementation of add new user methode

--- src/main/java/com/app/chat/controllers/ChatController.java
+++ src/main/java/com/app/chat/controllers/ChatController.java
@@ -4,6 +4,8 @@ import com.app.chat.models.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.broker.SimpleBrokerMessageHandler;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,6 +14,24 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage sendMsg(@Payload ChatMessage msg)
     {
+        return msg;
+    }
+
+
+    /**
+     * Handles requests to add a new user to the chat.
+     * The username is stored in the session attributes, and the message is broadcasted
+     * to all subscribers of the "/topic/public" destination.
+     * msg the chat message payload containing the username
+     * accessor the header accessor for accessing session attributes
+     * @return the chat message to be sent to the subscribers
+     */
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage msg,
+                               SimpMessageHeaderAccessor accessor)
+    {
+        accessor.getSessionAttributes().put("username", msg.getSender());
         return msg;
     }
 
